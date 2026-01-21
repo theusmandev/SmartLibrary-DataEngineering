ad83ef1b708838087b43e257cdba5335c707dba1
theusmandev <theusmandeveloper@gmail.com>
2026-01-22 01:29:08 +0500

    fix(etl): change data conversion output to DT_WSTR to match SQL NVARCHAR columns

--- database/create_stagging_table.sql
+++ database/create_stagging_table.sql
@@ -6,3 +6,12 @@ GO
 
 
 select * from stg.Novels_Excel
+
+
+USE SmartLibraryDB;
+GO
+
+-- Columns ko NVARCHAR (Unicode) mein tabdeel karein
+ALTER TABLE stg.Novels_Excel ALTER COLUMN Title NVARCHAR(500);
+ALTER TABLE stg.Novels_Excel ALTER COLUMN NovelLink NVARCHAR(1000);
+GO
